0bd2e68eadf4bbfa445fa146741ac942479e1f6d
SRUTHI S <srut21414.cs@rmkec.ac.in>
2024-03-14 20:39:04 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/') {
+        res.end('Hey Sruthi Sivasankararaj !!')
+    }
+
+    if(req.url === '/about') {
+        res.end('Hey, Its About Page !!')
+    }
+
+    res.end(`
+    <h1>Oops!</h1>
+    <p>We can't seem to find the page you are looking for!</p>
+    <a href="/">Back Home</a>
+    `)
+})
+
+server.listen(5000)
